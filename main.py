271e79b2ec8e4b3206665d0621acd375977d8009
LunaKamituki <163498223+LunaKamituki@users.noreply.github.com>
2024-09-05 11:36:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -208,11 +208,12 @@ def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
     print(check_cookie(yuki))
     return redirect("/word")
 
-@app.post("/certification")
+@app.get("/certification")
 def check_password(response: Response, q:str):
     if q == '罪刑法定主義':
         response.set_cookie(key="yuki", value="True",max_age=7*24*60*60)
-    return '/'
+        response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
+    return redirect("/")
     
 @app.get('/watch', response_class=HTMLResponse)
 def video(v:str,response: Response,request: Request,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
